66f10d8e80be5cc0597bd237e61b1c494be7375a
Jean Marcel <jeanvanzella91@gmail.com>
2022-07-13 19:33:37 -0300

    created 3 interfaces

---
+++ src/interfaces/carInterface.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { VehicleSchema } from './vehicleInterface';
+
+const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+export type Car = z.infer<typeof CarSchema>;
+
+export { CarSchema };
